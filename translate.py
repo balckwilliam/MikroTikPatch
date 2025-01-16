4d029612cdbab880e1a47888f673fd6fe79f9948
balckwilliam <32457598+balckwilliam@users.noreply.github.com>
2025-01-16 14:03:02 +0800

    Update translate.py
    Signed-off-by: balckwilliam <32457598+balckwilliam@users.noreply.github.com>

--- translate.py
+++ translate.py
@@ -46,7 +46,8 @@ def main(input_file, output_file):
     translated_text = ''
     for i in range(0, len(english_text.split('\n')), 20):
         text = '\n'.join(english_text.split('\n')[i:i+20])
-        translated_text += translate_text(text) + '\n'
+        translated_text += text + '\n'
+        #translated_text += translate_text(text) + '\n'
     
 
     if translated_text:
